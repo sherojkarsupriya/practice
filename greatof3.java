d585e4c71d7e35f0b6f763b88c6024d9bc264264
sherojkarsupriya <124039481+sherojkarsupriya@users.noreply.github.com>
2023-02-08 21:10:26 +0530

    greatof3.08/02

---
+++ greatof3.java
@@ -0,0 +1,26 @@
+class greatof3
+{
+public static void main(String args[])
+{
+int x=10,y=20,z=50;
+if(x>y)
+{
+if(x>z)
+{
+System.out.println("x is greater");
+}
+else
+{
+System.out.println("z is greater");
+}
+}
+else if(y>z)
+{
+System.out.println("y is greater");
+}
+else
+{
+System.out.println("z is greater");
+}
+}
+}
