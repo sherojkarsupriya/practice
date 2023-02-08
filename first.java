532de97d1d1c881bba788257c3cefa74202ccd9f
sherojkarsupriya <124039481+sherojkarsupriya@users.noreply.github.com>
2023-02-08 20:52:10 +0530

    practice.02/02

---
+++ first.java
@@ -0,0 +1,16 @@
+class second
+ {
+ public static void main(String arg[])
+  {
+    System.out.println("Hello World");
+    display();
+   second.supriya=new second();
+   supriya.greeting();
+ }
+public static void display(){
+   System.out.println("welcome to java");
+    }
+public void greeting(){
+    System.out.println("GOOD Morning");
+}
+}
