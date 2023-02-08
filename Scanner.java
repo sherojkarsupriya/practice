d5df79a4435841425a45d0ce324ef682b0f2ac45
sherojkarsupriya <124039481+sherojkarsupriya@users.noreply.github.com>
2023-02-08 22:23:34 +0530

    Scanner.06/02

---
+++ Scanner.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Conditonal
+{
+public static void main(String args[])
+{
+Scanner sc=new scanner(System.in);
+System.out.println("Hey user enter first value");
+int num1=sc.nextInt();
+System.out.println("Hey user enter second value");
+int num2=sc.nextInt();
+int addition=num1+num2;
+System.out.println("addition is"+addition);
+}
+}
