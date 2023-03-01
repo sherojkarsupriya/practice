5c3f771b08dc84db113a6f88ac072a822a445222
sherojkarsupriya <124039481+sherojkarsupriya@users.noreply.github.com>
2023-03-01 14:37:19 +0530

    pract.super key.01/03

---
+++ sbi.java
@@ -0,0 +1,19 @@
+class sbi extends axis
+{
+sbi()
+{
+super();//default constructor is created 
+System.out.println("this is SBI");
+}
+public static void main(String args[])
+{
+sbi s=new sbi();
+}
+}
+class axis 
+{
+axis()
+{
+System.out.println("this is Axis");
+}
+}
