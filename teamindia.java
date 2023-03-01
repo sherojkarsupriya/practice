55ab90ea209625c95d693c7e80090375d1ada755
sherojkarsupriya <124039481+sherojkarsupriya@users.noreply.github.com>
2023-03-01 14:34:49 +0530

    super class.01/03

---
+++ teamindia.java
@@ -0,0 +1,16 @@
+class teamindia extends msdhoni
+{
+ public static void main(String args[])
+ {
+    teamindia ti=new teamindia();
+    ti.trophies();
+ }
+ public void trophies()
+  {
+   System.out.println(super.trophies);
+  }
+}
+class msdhoni
+{
+ String trophies="Icc worldcup,championship cup, T20 world cup";
+}
